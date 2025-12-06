a7a947724ba510cbc37910938e2144524b2954ef
cristine jen <crsedano.student@asiancollege.edu.ph>
2025-12-06 12:42:07 +0800

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { supabase } from "./supabase-client";
 import "./App.css";
 
 export default function App() {
+
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState({ title: "", description: "" });
   const [newDescription, setNewDescription] = useState("");
